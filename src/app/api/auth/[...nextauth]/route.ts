779eca5d6b32c4a72816a19521e7611a0055d0f5
nestorjuarez <pruebanrj@hotmail.com>
2025-09-22 10:45:59 -0300

    cambio en login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -42,7 +42,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.role = user.role;
+        token.role = user.role!;
       }
       return token;
     },
